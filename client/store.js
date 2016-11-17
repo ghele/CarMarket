ae66ff2eaf5422b9485deed05071cb0ca453a42c
ghele <gheletus_raoul@yahoo.com>
2016-11-17 07:33:12 +0200

    Switch case moved to index.js inside an angular service

--- client/store.js
+++ client/store.js
@@ -7,8 +7,6 @@ let store = createStore(cartManipulation, applyMiddleware(thunk));
 
 console.log( loadInitialState );
 
-
 store.dispatch(loadInitialState());
-
 store.subscribe( () => console.log("state", store.getState() ) )
 // console.log("wpeoufh9oeh", store.getState());
