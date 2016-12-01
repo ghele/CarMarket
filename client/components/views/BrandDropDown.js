d1d63d65dbfc5669c5590fb05ba28ccb78a0c1d1
ghele <gheletus_raoul@yahoo.com>
2016-12-01 18:01:07 +0200

    Got the input refs

--- client/components/views/BrandDropDown.js
+++ client/components/views/BrandDropDown.js
@@ -3,13 +3,21 @@ import React from 'react';
 const BrandDropDown = React.createClass( {
   getData () {
     const raoul = this.props
+    const raoul1 = this.refs.brandName.value
     console.log("BrandDropDown",raoul);
+    console.log("BrandDropDownref",raoul1);
+  },
+  handleOnChange( ) {
+    console.log("Submitting the form");
+    console.log(this.refs.brandName.value);
+    const brandName = this.refs.brandName.value;
+    this.props.filterAfterBrandDropdown(brandName);
   },
   render( ) {
     return (
       <div>
         <label htmlFor="brandName">Select brand</label>
-        <select className="form-control">
+        <select className="form-control" ref="brandName" onChange={this.handleOnChange}>
             <option value="">Select</option>
             { !this.props.posts.isFetching ? this.props.posts.items.name.map( ( name, i ) => <option key={i}>{name}</option> ) : true }
         </select>
--- client/components/views/ModelDropDown.js
+++ client/components/views/ModelDropDown.js
@@ -5,11 +5,18 @@ const ModelDropDown = React.createClass( {
     const raoul = this.props
     console.log("ModelDropDown",raoul);
   },
+  handleOnChange( ) {
+    console.log("Submitting the form");
+    console.log(this.props.search.brandDropdown);
+    console.log(this.refs.modelName.value);
+    const modelName = this.refs.modelName.value;
+    this.props.filterAfterModelDropdown(modelName);
+  },
   render( ) {
     return (
       <div>
         <label htmlFor="modelName">Select model</label>
-        <select className="form-control">
+        <select className="form-control" ref="modelName" onChange={this.handleOnChange}>
             <option value="">Select</option>
             { !this.props.posts.isFetching ? this.props.posts.items.models.map( ( model ) => <option key={model.id}>{model.name}</option> ) : true }
         </select>
