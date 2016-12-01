d6e76087d85c30d49c3c4bf2bd0c18a6e91ddf26
ghele <gheletus_raoul@yahoo.com>
2016-12-01 14:20:14 +0200

    Fix bindActionCreators() first argument issue

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,7 @@
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as postsActionCreators from '../actions/actionCreators/posts';
-// import * as searchActionCreators from '../actions/actionCreators/search';
+import * as searchActionCreators from '../actions/actionCreators/search';
 import Main from './containers/Main';
 
 function mapStateToProps(state) {
@@ -14,7 +14,7 @@ function mapStateToProps(state) {
 console.log("mapStateToProps", mapStateToProps({}));
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators( postsActionCreators, dispatch);
+  return bindActionCreators( Object.assign( { }, postsActionCreators, searchActionCreators ), dispatch );
 }
 
 const App = connect(mapStateToProps, mapDispatchToProps)(Main);
